142f98f9294ec96e943e7d1396be0b0024bf435d
felicity <bogueyerif@gmail.com>
2024-03-21 10:08:35 +0100

    modification apporter

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,15 @@ const express=require('express');
 
 const app=express();
 
-const PORT = 5000;
 
-const URL_MONGO='mongodb://localhost:27017'
+const PORT = 7000;
+const URL_MONGO="mongodb://localhost:27017";
 
 
-mongoose.connect(`${URL_MONGO}/pharmacie/test1`).then(()=>{
+mongoose.connect(`${URL_MONGO}/pharmacieDB`).then(()=>{
 console.log('connecter-vous a la base se donnee ' +PORT)
 app.listen(PORT,()=>{
-    console.log('le server est lance sur le port' +PORT)
+    console.log('le server est lance sur le port'+PORT)
     })
 }).catch(error => {
     console.log(error);
