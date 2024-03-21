13079f0539a7fc5dcbb22b2c7ccb538a4ad86915
felicity <bogueyerif@gmail.com>
2024-03-21 10:49:53 +0100

    modification apporter sur le sever  app

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,21 @@
-import mongoose from 'mongoose';
+const mongoose =require('mongoose');
 const express=require('express');
 
 const app=express();
 
 
-const PORT = 5000;
+const PORT = 7000;
 
 mongoose.connect('mongodb://localhost:27017/pharmacieDB').then(()=>{
-     console.log('connecter-vous sur le server'+PORT)
-     app.listen(PORT,()=>{
-     console.log('le server est lance sur le port'+PORT)
-     })
-     }).catch(error=>{
+     console.log('connecter-vous sur le server' + PORT)
+     app.listen(PORT, () => {
+     console.log('le server est lance sur le port' + PORT)
+     });
+     }).catch(error => {
         console.log(error);
         throw new Error("erreur de connection sur le server") ;
         }
-     
-    
-)
+     )
 
 
 
